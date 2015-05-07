dcc97b11ab62a3d13e537f998383ea8a98579287
Frank Thielen <froonge@web.de>
2015-05-07 13:09:01 +0200

    Tomcat Support for CDI

--- src/main/java/org/bonn/se2/shopaholics/shopinshop/dao/UserDAOImpl.java
+++ src/main/java/org/bonn/se2/shopaholics/shopinshop/dao/UserDAOImpl.java
@@ -8,8 +8,12 @@ package org.bonn.se2.shopaholics.shopinshop.dao;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import javax.annotation.Resource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.bonn.se2.shopaholics.shopinshop.model.User;
 
@@ -19,12 +23,7 @@ import org.bonn.se2.shopaholics.shopinshop.model.User;
  */
 @RequestScoped
 public class UserDAOImpl implements UserDAO, Serializable {
-    
-    
-    @Resource
-    private DataSource shopDS;
-    
-    
+   
     @Override
     public User getUser(String username, String password) {
 
@@ -39,67 +38,31 @@ public class UserDAOImpl implements UserDAO, Serializable {
             
             */
             
-            User user = null;
-            
-            try {
-                Connection con = shopDS.getConnection();
-                ResultSet res = con.createStatement().executeQuery("SELECT * FROM shop.user where username = '" + username + "';");
-                while(res.next()) {
+                    User user = null;
+
+        try {
+            InitialContext cxt = new InitialContext();
+
+            DataSource ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/shopDB");
+            ResultSet res = ds.getConnection().createStatement().executeQuery("SELECT * FROM shop.user where username = '" + username + "';");
+
+            while (res.next()) {
                 String pw = res.getString("password").trim();
-                    if (pw.equals(password)) {
-                        user = new User();
-                        user.setUsername(username);
-                        user.setVorname(res.getString("vorname"));
-                        user.setNachname(res.getString("nachname"));
-                    }
-                }
-            }
-            catch (Exception ex) {
-            
-            }
-            
-            return user;
-            
-            
-            
-            /*
-        
-            User user = null;
-        
-            try {
-            
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            String url = "jdbc:postgresql://dumbo.inf.fh-bonn-rhein-sieg.de:5432/kmisin2s";
-            Properties pros = new Properties();
-            pros.setProperty("user", "kmisin2s");
-            pros.setProperty("password", "kmisin2s");
-            
-            Connection con = DriverManager.getConnection(url, pros);
-            
-            Statement st;
-            st = con.createStatement();
-            
-            ResultSet rs = st.executeQuery("SELECT * FROM shop.user;");
-            
-            while(rs.next()) {
-                String pw = rs.getString("password");
-                
                 if (pw.equals(password)) {
                     user = new User();
-                    user.setVorname(rs.getString("vorname"));
-                    user.setNachname(rs.getString("nachname"));
                     user.setUsername(username);
+                    user.setVorname(res.getString("vorname"));
+                    user.setNachname(res.getString("nachname"));
                 }
-            } 
+            }
+
+        } catch (NamingException ex) {
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        
         return user;
-                    
-                    */
-                
                
     }
     
