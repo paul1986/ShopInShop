13b5b73b07c7e5478568762391579f37d703e8f5
Lucas Prochnow <lucasprochnow@gmx.net>
2015-05-10 15:43:56 +0200

    produktübersicht vorläufig eine Tabelle mit Bean Container

--- src/main/java/org/bonn/se2/shopaholics/shopinshop/ui/views/ProductsView.java
+++ src/main/java/org/bonn/se2/shopaholics/shopinshop/ui/views/ProductsView.java
@@ -5,11 +5,14 @@
  */
 package org.bonn.se2.shopaholics.shopinshop.ui.views;
 
+import com.google.gwt.dom.client.Style;
 import com.vaadin.cdi.CDIView;
+import com.vaadin.data.util.BeanContainer;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 import java.util.List;
 import javax.inject.Inject;
@@ -31,32 +34,15 @@ public class ProductsView extends LoginNeededComponent {
     public void viewEntered(ViewChangeListener.ViewChangeEvent event) {
         VerticalLayout verticalLayout = new VerticalLayout();
         setCompositionRoot(verticalLayout);
-//        verticalLayout.addComponent(new Label("Liste der Produkte"));
-//        GridLayout columnNames = new GridLayout(4, 1);
-//        column.addComponent(new Label());
-//        
-//        List<Product> result = poc.getAllProducts();
-//        for(int i = 0; i < result.size(); ++i){
-//            GridLayout grid = new GridLayout(4, 1);
-//            grid.addComponent(new Label());
-//
-//            verticalLayout.addComponent(grid);
-//        }
 
-        //initialisiere
         List<Product> result = poc.getAllProducts();
-        GridLayout myGrid = new GridLayout(4, result.size() + 1);
-
-        //set Column Names
-        myGrid.addComponent(new Label("Name"));
-        myGrid.addComponent(new Label("Beschreibung"));
-        myGrid.addComponent(new Label("Verfügbarkeit"));
-        myGrid.addComponent(new Label("Preis"));
-//
-//        myGrid.setWidth("900px");
-//        myGrid.setColumnExpandRatio(1, 1);
-
-        verticalLayout.addComponent(myGrid);
+        
+        BeanContainer<Integer, Product> data = new BeanContainer<Integer, Product>(Product.class);
+        data.setBeanIdProperty("art_nr");
+        data.addAll(result);
+        Table table = new Table("Produktübersicht", data);
+        table.setSelectable(true);
+        verticalLayout.addComponent(table);
     }
 
 }
